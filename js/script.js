9c402cfcb969b6fb6556474532366f1482fa292a
Vitaliy <v0637795486@gmail.com>
2017-02-27 14:01:25 +0200

    Icon

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.onclick = function(e) {
 for (var i=0; i < img.length; i++) {
     img[i].onclick = function() {
         modal.classList.remove("hidden");
-        imgModal.style.backgroundImage = "url('foto2.jpg')";
+        imgModal.style.backgroundImage = "url('../img/foto1.jpg')";
     };
 }
 
@@ -25,3 +25,38 @@ for (var i=0; i < img.length; i++) {
 //     var urlString = 'url(img/' + imagePrefix + '.jpg)';
 //     document.getElementById(imgModal).style.backgroundImage =  urlString;
 // }
+
+
+// var elLarge = document.getElementsByClassName('large-img');
+// for (var t=0; t < elLarge.length; t++) {
+//     elLarge[t].onmouseover = function() {
+//         elLarge.style.display = "none";};
+//
+// }
+// function mouseOver() {
+//     elLarge.style.color = "red";
+// }
+// var container = document.getElementById('icon');
+// var elements = container.getElementsByClassName('large-img');
+// for(var x = 0; x < elements.length; x++) {
+//     elements[x].addEventListener("mouseover",function(){
+//         this.attribute("src").mat = "img/LinkedInW.png";
+//
+//     },false);
+//     elements[x].addEventListener("mouseout",function(){
+//         this.src = "img/LinkedIn.png";
+//
+//     },false);
+// }
+$(function() {
+    $("#icon").find("img")
+        .mouseover(function() {
+            var src = $(this).attr("src").match(/[^\.]+/) + "W.png";
+            $(this).attr("src", src);
+        })
+        .mouseout(function() {
+            var src = $(this).attr("src").replace("W.png", ".png");
+            $(this).attr("src", src);
+        });
+});
+
